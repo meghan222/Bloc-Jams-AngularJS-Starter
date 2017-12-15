703ff06f2ffce02c5c50064d0d35fbc2527849a3
meghan222 <meghan222@github.com>
2017-12-15 14:14:50 -0600

    assignment 6 services completed

--- app/scripts/controllers/CollectionCtrl.js
+++ app/scripts/controllers/CollectionCtrl.js
@@ -1,13 +1,13 @@
 (function() {
-     function CollectionCtrl() {
-       this.albums = [];
-     for (var i=0; i < 12; i++) {
-         this.albums.push(angular.copy(albumPicasso));
-     }
+     function CollectionCtrl(Fixtures) {
+        this.albums = [];
 
+        for (var i=0; i < 12; i++) {
+            this.albums.push(Fixtures.getAlbum());
+        }
      }
 
      angular
          .module('blocJams')
-         .controller('CollectionCtrl', CollectionCtrl);
+         .controller('CollectionCtrl', ['Fixtures', CollectionCtrl]);
  })();
